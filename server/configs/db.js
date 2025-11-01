bde38583b4b9912ce4269aadf4847a6016db642c
shashank kumar <shashankmishra00026@gmail.com>
2025-11-01 17:45:18 +0530

    change in monogo connection and added frontend url in backend cors and updated vite_backend_url in frontebd

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
-    try {
-        mongoose.connection.on('connected', ()=> console.log("Database Connected")
-        );
-        await mongoose.connect(`${process.env.MONGODB_URI}/greencart`)
-    } catch (error) {
-        console.error(error.message);
-    }
-}
-
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
